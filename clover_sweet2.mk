8d82ffc005ee1a1aa9487f2c8a442d7028f28880
DarkKiller28 <oyunbaskani7728@gmail.com>
2026-05-10 13:04:31 +0300

    Initilize Clover 3.x QPR2

---
+++ clover_sweet2.mk
@@ -0,0 +1,37 @@
+#
+# Copyright (C) 2021-2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from device.
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+# Inherit some common Clover stuff.
+$(call inherit-product, vendor/clover/config/common_full_phone.mk)
+
+PRODUCT_BRAND := Redmi
+PRODUCT_DEVICE := sweet2
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_MODEL := 2209116AG
+PRODUCT_NAME := clover_sweet2
+PRODUCT_SYSTEM_DEVICE := sweet
+PRODUCT_SYSTEM_NAME := sweet_global2
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="sweet_global2-user 13 TKQ1.221114.001 V816.0.13.0.THGMIXM release-keys" \
+    BuildFingerprint=Redmi/sweet_global2/sweet:13/TKQ1.221114.001/V816.0.13.0.THGMIXM:user/release-keys \
+    DeviceName=$(PRODUCT_SYSTEM_DEVICE) \
+    DeviceProduct=$(PRODUCT_SYSTEM_NAME)
+# Clover Flags
+CLOVER_BUILDTYPE := Community
+CLOVER_MAINTAINER := ℝ𝕖𝕧𝕖𝕣𝕤𝕖𝕕ℝ𝕦𝕝𝕖𝕣༆𖣘(Soner)
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
